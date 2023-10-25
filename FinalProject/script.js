fec5b9ee32e28d73b07f38a09ab5745c3695305c
bentonah <han21002@byui.edu>
2023-10-24 20:39:12 -0600

    f

--- FinalProject/script.js
+++ FinalProject/script.js
@@ -1,36 +1,44 @@
+// dataFetcher.js
+export async function fetchMemes() {
+    const response = await fetch("https://api.imgflip.com/get_memes");
+    const data = await response.json();
+    const memes = data.data.memes;
+    const randomIndex = Math.floor(Math.random() * memes.length);
+    return memes[randomIndex].url;
+}
+
+export async function fetchInspirationalQuote() {
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+    return `${data.content} - ${data.author}`;
+}
+
+// main.js
+import { fetchMemes, fetchInspirationalQuote } from './dataFetcher.js';
+
 const funnyButton = document.getElementById("funnyButton");
 const inspirationalButton = document.getElementById("inspirationalButton");
 const contentDiv = document.getElementById("content");
 
-function fetchMeme() {
-    return fetch("https://api.imgflip.com/get_memes")
-        .then(response => response.json())
-        .then(data => {
-            const memes = data.data.memes;
-            const randomIndex = Math.floor(Math.random() * memes.length);
-            return memes[randomIndex].url;
-        });
-}
-
-function fetchInspirationalQuote() {
-    return fetch("https://api.quotable.io/random")
-        .then(response => response.json())
-        .then(data => {
-            return data.content + " - " + data.author;
-        });
-}
-
 function displayContent(content) {
     contentDiv.innerHTML = "";
-    if (content.includes(".jpg") || content.includes(".png") || content.includes(".gif")) {
-        const img = document.createElement("img");
-        img.src = content;
-        img.alt = "Meme";
-        contentDiv.appendChild(img);
+    if (Array.isArray(content)) {
+        content.forEach(item => {
+            const p = document.createElement("p");
+            p.textContent = item;
+            contentDiv.appendChild(p);
+        });
     } else {
-        const p = document.createElement("p");
-        p.textContent = content;
-        contentDiv.appendChild(p);
+        if (content.includes(".jpg") || content.includes(".png") || content.includes(".gif")) {
+            const img = document.createElement("img");
+            img.src = content;
+            img.alt = "Meme";
+            contentDiv.appendChild(img);
+        } else {
+            const p = document.createElement("p");
+            p.textContent = content;
+            contentDiv.appendChild(p);
+        }
     }
 
     const newContentButton = document.createElement("button");
@@ -42,25 +50,26 @@ function displayContent(content) {
     contentDiv.appendChild(newContentButton);
 }
 
-function fetchAndDisplayContent(fetchFunction) {
-    fetchFunction()
-        .then(content => {
-            displayContent(content);
-        })
-        .catch(error => console.error(error));
+async function fetchAndDisplayContent(fetchFunction) {
+    try {
+        const content = await fetchFunction();
+        displayContent(content);
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 function displayRandomQuoteOrImage() {
     const random = Math.random();
     if (random < 0.5) {
-        fetchAndDisplayContent(fetchMeme);
+        fetchAndDisplayContent(fetchMemes);
     } else {
         fetchAndDisplayContent(fetchInspirationalQuote);
     }
 }
 
 funnyButton.addEventListener("click", () => {
-    fetchAndDisplayContent(fetchMeme);
+    fetchAndDisplayContent(fetchMemes);
 });
 
 inspirationalButton.addEventListener("click", () => {
